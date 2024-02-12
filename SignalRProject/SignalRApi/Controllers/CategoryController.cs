469106f5f29f99899731fe456f01cc3c25d035c5
kaansarr <kaansar33@gmail.com>
2024-02-12 16:53:21 +0300

    Kategori Api Bitimi

--- SignalRProject/SignalRApi/Controllers/CategoryController.cs
+++ SignalRProject/SignalRApi/Controllers/CategoryController.cs
@@ -38,7 +38,7 @@ namespace SignalRApi.Controllers
 			return Ok("Kategori Eklendi");
 		}
 
-		[HttpDelete]
+		[HttpDelete("{id}")]
 
 		public IActionResult DeleteCategory(int id)
 		{
@@ -47,7 +47,7 @@ namespace SignalRApi.Controllers
 			return Ok("Kategori Silindi");
 		}
 
-		[HttpGet("GetCategory")]
+		[HttpGet("{id}")]
 		public IActionResult GetCategory(int id)
 		{
 			var value = _categoryService.TGetByID(id);
