3d3e5042b5619b596ac5a5ed6bb7d18a80c3a3f8
kaansarr <kaansar33@gmail.com>
2024-04-01 03:01:57 +0300

    up

--- SignalRProject/SignalRApi/Hubs/SignalRHub.cs
+++ SignalRProject/SignalRApi/Hubs/SignalRHub.cs
@@ -11,17 +11,19 @@ namespace SignalRApi.Hubs
 		private readonly IOrderService _orderService;
 		private readonly IMoneyCaseService _moneyCaseService;
 		private readonly IMenuTableService _menuTableService;
+		private readonly IBookingService _bookingService;
 
-		public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService)
-		{
-			_categoryService = categoryService;
-			_productService = productService;
-			_orderService = orderService;
-			_moneyCaseService = moneyCaseService;
-			_menuTableService = menuTableService;
-		}
+        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService, IBookingService bookingService)
+        {
+            _categoryService = categoryService;
+            _productService = productService;
+            _orderService = orderService;
+            _moneyCaseService = moneyCaseService;
+            _menuTableService = menuTableService;
+            _bookingService = bookingService;
+        }
 
-		public async Task SendStatistic()
+        public async Task SendStatistic()
 		{
 			var value = _categoryService.TCategoryCount();
 			await Clients.All.SendAsync("ReceiveCategoryCount",value);
@@ -103,7 +105,11 @@ namespace SignalRApi.Hubs
 
 
 
-
+		public async Task GetBookingList()
+		{
+			var values = _bookingService.TGetListAll();
+			await Clients.All.SendAsync("ReceiveBookingList", values);
+		}
 
 
 
--- SignalRProject/SignalRWebUI/Controllers/BasketsController.cs
+++ SignalRProject/SignalRWebUI/Controllers/BasketsController.cs
@@ -28,7 +28,16 @@ namespace SignalRWebUI.Controllers
 			return View();
 		}
 
-		
+		public async Task<IActionResult> DeleteBasket(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.DeleteAsync($"https://localhost:7095/api/Basket/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return NoContent();
+		}
 
 	}
 }
